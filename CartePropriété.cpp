6d7c5fddd07242bc6a3c4d090547f3fc847603ec
Hugo DIVET <hugo.divet@epsi.fr>
2020-03-24 17:12:29 +0100

    Début des cartes

--- CartePropriété.cpp
+++ CartePropriété.cpp
@@ -1,1 +1,16 @@
 #include "CartePropriété.h"
+#include <iostream>
+#include <string>
+
+CartePropriété::CartePropriété()
+{
+
+}
+
+void CartePropriété::Attribuer(string nom, int loyer, string Nomcouleur, int proprietaire) {
+	this->nom = nom;
+	this->loyer = loyer;
+	this->Nomcouleur = Nomcouleur;
+	this->proprietaire = 0;
+}
+
