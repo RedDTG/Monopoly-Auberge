412f403fd22892b04d79b07833361a13e4937f4f
theob <theo.brutus@gmail.com>
2020-04-02 14:39:09 +0200

    Plateau

---
+++ Plateau.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <string>
+#include <iomanip>
+Class Grille{
+	private:
+		int largeur = 8;
+		int hauteur = 5;
+		vector<int> tableau;
+
+
+	public:
+		Grille(int largeur, int hauteur) {
+			this->largeur = largeur;
+			this->hauteur = hauteur;
+			tableau.resize(largeur * hauteur, 0);
+		}
+
+		int recuperer(int x, int y) {
+			return grille[y * largeur + x];
+		}
+}
+
+int main()
+{
+	Grille grille(10, 10) :
+
+	cout << grille.recuperer(4, 4) << endl;
+
+	return 0;
+}
