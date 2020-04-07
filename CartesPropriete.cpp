dc5177646c594ab83185239991138854786447af
Hugo Divet <hugo.divet@epsi.fr>
2020-04-08 00:51:43 +0200

    Carte Propriétés avec de l'héritage
    Ajout héritage sur nos cartes de propriétés.

--- CartesPropriete.cpp
+++ CartesPropriete.cpp
@@ -12,13 +12,10 @@ void color(int t, int f)
 	SetConsoleTextAttribute(H, f * 16 + t);
 }
 
-CartePropriete::CartePropriete(string nom, int prixAchat, int loyer, string Nomcouleur, int type, int idcouleur) {
+CartePropriete::CartePropriete(string nom, int prixAchat, int loyer, int type) {
 	this->nom = nom;
 	this->loyer = loyer;
-	this->Nomcouleur = Nomcouleur;
-	this->proprietaire = 0;
 	this->type = type;
-	this->idcouleur = idcouleur;
 	this->prixAchat = prixAchat;
 }
 
@@ -27,8 +24,71 @@ void CartePropriete::afficherCarteProp()
 	color(this->idcouleur,0);
 	cout << "Nom de la carte : " << this->nom << endl;
 	cout << "Prix de la propriete: " << this->prixAchat << endl;
-	cout << "Prix du loyer : " << this->loyer << " | Type de la propriete : " << this->type << " |  Couleur : " << this->Nomcouleur << endl;
+	cout << "Prix du loyer : " << this->loyer << " | Type de la propriete : " << this->type << " |  Couleur : " << this->NomCouleur << endl;
 	cout << endl;
 	color(15,0);
+}
+
+int CartePropriete::getidCouleur() {
+	return idcouleur;
+}
+
+Joueur* CartePropriete::getProprio() { return proprietaire; }
+string CartePropriete::getNom() { return nom; }
+int CartePropriete::getLoyer() { return loyer; }
+int CartePropriete::getPrix() { return prixAchat; }
+void CartePropriete::setProprio(Joueur* proprio) { this->proprietaire = proprio; }
+
+void CartePropriete::setidCouleur(int idcouleur) {
+	this->idcouleur = idcouleur;
+}
+
+string CartePropriete::getNomCouleur() {
+	return NomCouleur;
+}
+
+void CartePropriete::setNomCouleur(string NomCouleur) {
+	this->NomCouleur = NomCouleur;
+}
+
+Marron::Marron(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
+}
+Gare::Gare(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
+}
+Compagnie::Compagnie(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
+}
+BleuClair::BleuClair(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
+}
+Rose::Rose(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
+}
+Orange::Orange(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
+}
+Rouge::Rouge(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
+}
+Jaune::Jaune(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
+}
+Vert::Vert(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
+}
+BleuFonce::BleuFonce(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {
+	setidCouleur(idcouleur);
+	setNomCouleur(NomCouleur);
 }
 
--- CartesPropriete.h
+++ CartesPropriete.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include "Joueur.h"
 using namespace std;
 
 class CartePropriete
@@ -8,8 +9,8 @@ class CartePropriete
 private:
 	string nom;
 	int loyer;
-	string Nomcouleur;
-	int proprietaire;
+	string NomCouleur;
+	Joueur* proprietaire;
 	int idcouleur;
 	int type;
 	int prixAchat;
@@ -19,9 +20,68 @@ private:
 
 
 public:
-	CartePropriete(string nom, int prixAchat, int loyer, string Nomcouleur, int type, int idcouleur);
+	CartePropriete(string nom, int prixAchat, int loyer, int type);
 
 	void afficherCarteProp();
+	void setProprio(Joueur* proprio);
+	void setidCouleur(int idcouleur);
+	int getidCouleur();
+	int getPrix();
+	Joueur* getProprio();
+	string getNom();
+	int getLoyer();
+	void setNomCouleur(string NomCouleur);
+	string getNomCouleur();
 
 
 };
+
+class Marron : public CartePropriete {
+public:	
+	Marron(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom,prixAchat,loyer,type) {}
+};
+
+class Gare : public CartePropriete {
+public:
+	Gare(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {}
+};
+class Compagnie : public CartePropriete {
+public:
+	Compagnie(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {}
+};
+
+class BleuClair : public CartePropriete {
+public:
+	BleuClair(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {}
+};
+
+class Rose : public CartePropriete {
+public:
+	Rose(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {}
+};
+
+class Orange : public CartePropriete {
+public:
+	Orange(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {}
+};
+
+class Rouge : public CartePropriete {
+public:
+	Rouge(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {}
+};
+
+class Jaune : public CartePropriete {
+public:
+	Jaune(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {}
+};
+
+class Vert : public CartePropriete {
+public:
+	Vert(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {}
+};
+
+class BleuFonce : public CartePropriete {
+public:
+	BleuFonce(string nom, int prixAchat, int loyer, string NomCouleur, int type, int idcouleur) : CartePropriete(nom, prixAchat, loyer, type) {}
+};
+
